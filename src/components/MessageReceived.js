1af23948377438f866dd381054483aaf6a4767e5
Jennifer Wjertzoch <jennifer.wjertzoch@jvm.de>
2021-07-17 23:16:35 +0200

    Add moments.js to validate and display timestamps in javascript

--- src/components/MessageReceived.js
+++ src/components/MessageReceived.js
@@ -1,9 +1,11 @@
+import moment from "moment";
+
 export default function MessageReceived({ message }) {
     return (
         <div className="card">
             <p>{message.author}</p>
             <h3>{message.message}</h3>
-            <p>{message.timestamp}</p>
+            <p>{moment(message.timestamp).format('LLL')}</p>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
+
 import './index.css';
 import App from './App';
+
+moment().format('LLL');    // July 17, 2021 11:13 PM
 
 ReactDOM.render(
   <React.StrictMode>
